2d96bc494547db81b0edd8d2a55dcf58406a88cb
Roboron3042 <robertoms258@gmail.com>
2022-01-30 21:27:28 +0100

    libs=>lib

--- cmake/SimutransCompileOptions.cmake
+++ cmake/SimutransCompileOptions.cmake
@@ -59,7 +59,7 @@ set_property(CACHE SIMUTRANS_MSG_LEVEL PROPERTY STRINGS 0 1 2 3 4)
 
 if(OPTION_BUNDLE_LIBRARIES AND UNIX AND NOT APPLE)
     set(CMAKE_BUILD_WITH_INSTALL_RPATH TRUE)
-    set(CMAKE_INSTALL_RPATH "\$ORIGIN/libs;")
+    set(CMAKE_INSTALL_RPATH "\$ORIGIN/lib")
     # otherwise RUNPATH will be set instead of RPATH, which can lead to issues
 	add_link_options("-Wl,--disable-new-dtags")
 endif()
